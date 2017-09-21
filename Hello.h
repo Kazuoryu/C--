68fa19694310b5d7c8c6b63c942388345643df7e
Diego <DiegoGlezAbreu@gmail.com>
2017-09-21 12:59:46 +0100

    Hello C++
    It is a first contact with c++

---
+++ Hello.h
@@ -0,0 +1,13 @@
+//Hello.cpp:
+#include "stdafx.h"
+#include <iostram>
+#include <string>
+
+int_tmain(int argc, _ TCHAR* argv[])
+{
+	std::cout << "type your name" << std::endl;
+	std::string name;
+	std:: cin >> name;
+	std::cout << "Hello, " << name << std::endl;
+	return 0
+}
